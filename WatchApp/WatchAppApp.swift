7f13cd5456b5203a5177146203e3cdd562b6883f
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-05-12 14:16:53 +0200

    Initial Commit

---
+++ WatchApp/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp
+//
+//  Created by Nicholas Samuelsson Jeria on 2025-05-12.
+//
+
+import SwiftUI
+
+@main
+struct WatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
