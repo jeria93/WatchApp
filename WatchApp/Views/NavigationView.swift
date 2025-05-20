e23b53cc72e085033760fb3769eeee675f3b5c07
ElDavido81 <eldavido81@gmail.com>
2025-05-20 14:34:35 +0200

    Navbar

---
+++ WatchApp/Views/NavigationView.swift
@@ -0,0 +1,48 @@
+//
+//  NavigationView.swift
+//  WatchApp
+//
+//  Created by David Kalitzki on 2025-05-20.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    
+    @State private var selection = 2
+    
+    var body: some View {
+        
+        TabView(selection:$selection){
+            VStack{
+                Text("WatchList")
+            }
+            .tabItem{
+                Image("pop_black_icon1x")
+                Text("WatchList")
+            }
+            .tag(1)
+            VStack{
+                MovieListView()
+                Text("")
+            }
+            .tabItem{
+                Image("movies_black_1x")
+                Text("Movies")
+            }
+            .tag(2)
+            VStack{
+                Text("Edit profile")
+            }
+            .tabItem{
+                Image(systemName: "person.fill")
+                Text("Profile")
+            }
+            .tag(3)
+        }
+    }
+}
+
+#Preview {
+    NavigationView()
+}
