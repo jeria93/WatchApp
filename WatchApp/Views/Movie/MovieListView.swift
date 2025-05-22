657913b95ba02c71a9bd77a2c4ebb07d1c645d7d
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-05-22 12:30:28 +0200

    fix: resolved merge conflict in MovieListView.swift

--- WatchApp/Views/Movie/MovieListView.swift
+++ WatchApp/Views/Movie/MovieListView.swift
@@ -12,8 +12,9 @@ struct MovieListView: View {
     @StateObject private var viewModel = MovieViewModel()
     @StateObject private var firestoreVM = FirestoreViewModel()
     @ObservedObject var authVM: AuthViewModel
-    
+
     var body: some View {
+        NavigationStack {
             VStack {
                 SearchBarView(text: $viewModel.searchText) {
                     Task {
@@ -24,7 +25,7 @@ struct MovieListView: View {
                         }
                     }
                 }
-                
+
                 Picker("Type", selection: $viewModel.selectedType) {
                     ForEach(ContentType.allCases) { type in
                         Text(type.rawValue).tag(type)
@@ -35,35 +36,33 @@ struct MovieListView: View {
                 .onChange(of: viewModel.selectedType) { _ in
                     Task { await viewModel.fetchTrendingContent() }
                 }
-                
+
                 if !viewModel.movies.isEmpty {
                     Text(viewModel.searchText.isEmpty ? "Trending now" : "\(viewModel.totalResults) results found")
                         .font(.subheadline)
                         .foregroundColor(.popcornYellow)
                         .padding(.top, 10)
                 }
-                
+
                 Group {
                     if viewModel.isLoading {
                         LoadingView()
-                        
                     } else if let error = viewModel.errorMessage {
                         ErrorView(message: error)
-                        
                     } else if viewModel.movies.isEmpty {
                         EmptyStateView(searchText: viewModel.searchText)
                             .frame(maxHeight: .infinity)
-                        
                     } else {
                         ContentListView(movies: viewModel.movies, contentType: viewModel.selectedType) { movie in
-                            Task { await firestoreVM.saveMovie(movie)}
+                            Task {
+                                await firestoreVM.saveMovie(movie)
+                            }
                         }
                     }
                 }
             }
             .background(Color.BG.ignoresSafeArea(.all))
             .navigationTitle("🎬 Trending Movies")
-           	.task { await viewModel.fetchTrendingContent() }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
@@ -75,7 +74,9 @@ struct MovieListView: View {
                     }
                 }
             }
-
+            .task {
+                await viewModel.fetchTrendingContent()
+            }
         }
     }
 }
