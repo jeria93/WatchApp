f42b4f3d89eb658cc2088053b532ab71bcf3478b
FridaEriksson95 <fridaeriiksson@gmail.com>
2025-06-04 15:52:55 +0200

    chore: untrack GoogleService-Info.plist

---
+++ WatchApp/Views/RandomPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  RandomPickerView.swift
+//  WatchApp
+//
+//  Created by Frida Eriksson on 2025-06-04.
+//
+
+import SwiftUI
+
+struct RandomPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RandomPickerView()
+}
